8ba8da3d6c57289d930a36d2433c0db7b73cdc73
Lori B <47092293+loribeth18@users.noreply.github.com>
2019-06-21 20:27:44 -0500

    updated

--- website/static/js/app.js
+++ website/static/js/app.js
@@ -8,7 +8,7 @@ function buildMetadata(city) {
 
     PANEL.html("");
 
-    Object.entries(data[0]).forEach(([key,value]) =>{ //don't worry about it
+    Object.entries(data[0]).forEach(([key,value]) =>{
       PANEL.append("h6").text(`${key}: ${value}`);
     })
     
@@ -16,31 +16,32 @@ function buildMetadata(city) {
 }
   
 
-function buildCharts(sample) {
+function buildCharts(city) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
   var url = `/cityCrime2/${city}`; 
   d3.json(url).then((data) =>{
-    // @TODO: Build a Bubble Chart using the sample data
-    const city = data.city;
-    const crimetype = data.type;
-    //const 2010 = data.2010;
+
+    // @TODO: Build a Chart using the sample data
+    // const city = data.city;
+    // const crimetype = data.type;
+    const year = ["2010","2011","2012","2013","2014","2015","2016"]
+    const yearValues = [data["2010"],data["2011"],data["2012"],data["2013"],data["2014"],data["2015"],data["2016"]]
 
     var bardata = [{
-      type: 'line',
-      //name: year,
-      x: ['2010','2011','2012','2013','2014','2015','2016'],
-      y: 'City-data.com crime index (higher means more crime, U.S. average = 280...'
-      //labels: year,
+      //name: city,
+      x: year,
+      y: [642, 596, 581, 455, 385, 349,369],
+      //data.map(row => row.["2016"]),
+      //text: data.map(row => row.type),
+      type: 'bar',
       //hovertext: otu_labels.slice(0,10),
       //hoverinfo: "hovertext",
-       
     }];
 
-    
-    var barlayout = {
+      var barlayout = {
       margin: {t:0,1:0}
-    };
+      };
     
     Plotly.plot('bar', bardata, barlayout);
   });
@@ -61,7 +62,7 @@ function init() {
 
     // Use the first sample from the list to build the initial plots
      const firstCity = dataPop[0];
-     //buildCharts(firstCity.city);
+     buildCharts(firstCity.city);
      buildMetadata(firstCity.city);
   });
 }
@@ -70,7 +71,7 @@ function optionChanged(newCity) {
   // Fetch new data each time a new sample is selected
   buildCharts(newCity);
   buildMetadata(newCity);
-  alert("You chose " + newCity + "! Wow!")
+  //alert("You chose " + newCity + "! Wow!")
 }
 
 // Initialize the dashboard
